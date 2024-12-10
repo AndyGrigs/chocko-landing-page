5eaf6ca0504b6e0940e0186c95f9fab9fe894474
Andriy <andygrigs88@gmail.com>
2024-12-10 17:25:37 +0100

    modal

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// Get the modal element
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("write-comment");
+// Open the modal
+document.getElementById("write-comment").addEventListener("click", function () {
+    document.getElementById("comment-modal").style.display = "block";
+  });
 
-// Get the span element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Close the modal
+  document.getElementById("close-modal").addEventListener("click", function () {
+    document.getElementById("comment-modal").style.display = "none";
+  });
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // Close modal when clicking outside of it
+  window.addEventListener("click", function (event) {
+    const modal = document.getElementById("comment-modal");
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 
-// When the user clicks on the span (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+  // Handle form submission
+  document.getElementById("comment-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Your comment has been submitted!");
+    document.getElementById("comment-modal").style.display = "none";
+  });
